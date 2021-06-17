09a55ea185a2de4dfac78a75c3902102a2ee2dd1
huangjunlin <573930212@qq.com>
2021-06-17 14:20:23 +0800

    commit

--- config-client/src/main/java/com/config/client/Application.java
+++ config-client/src/main/java/com/config/client/Application.java
@@ -1,16 +1,15 @@
+package com.config.client;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @author junlin_huang
- * @create 2021-06-17 1:22 PM
+ * @create 2021-06-17 2:00 PM
  **/
 
-@EnableConfigServer
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class).web(true).run(args);
     }
---
+++ config-server/src/main/java/com/config/server/Application.java
@@ -0,0 +1,20 @@
+package com.config.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author junlin_huang
+ * @create 2021-06-17 1:22 PM
+ **/
+
+@EnableConfigServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+
+}
